5215658026ac17bcb6225794ae57e101ade9a06f
Alan Heywood <alan@softweb.net.au>
2023-01-03 11:51:14 +1000

    Add a failing test to cover auto enum generation

---
+++ test/attribute_test.exs
@@ -0,0 +1,32 @@
+defmodule AshGraphql.AttributeTest do
+  use ExUnit.Case, async: false
+
+  setup do
+    Application.delete_env(:ash_graphql, AshGraphql.Test.Api)
+
+    on_exit(fn ->
+      try do
+        AshGraphql.TestHelpers.stop_ets()
+      rescue
+        _ ->
+          :ok
+      end
+    end)
+  end
+
+  test "atom attribute with one_of constraints has enums automatically generated" do
+    {:ok, %{data: data}} =
+      """
+      query {
+        __type(name: "Visibility") {
+          enumValues {
+            name
+          }
+        }
+      }
+      """
+      |> Absinthe.run(AshGraphql.Test.Schema)
+
+    assert data["__type"]
+  end
+end
